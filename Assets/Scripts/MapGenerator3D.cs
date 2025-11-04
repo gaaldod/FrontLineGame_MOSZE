d7da4864686dc789a1030e6f36fa9f59825719b3
Wallorzo <lencike23@gmail.com>
2025-11-04 22:21:21 +0100

    feat: map generalas

--- Assets/Scripts/MapGenerator3D.cs
+++ Assets/Scripts/MapGenerator3D.cs
@@ -1,8 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class HexMap3D : MonoBehaviour
 {
-    public GameObject hexPrefab;
+    public GameObject groundHexPrefab;
+    public GameObject castleHexPrefab;
     public int width = 8;
     public int height = 8;
     public float hexSize = 1f;
@@ -14,19 +15,33 @@ public class HexMap3D : MonoBehaviour
 
     void GenerateMap()
     {
-        float xOffset = hexSize * 0.75f;
-        float zOffset = hexSize * 0.8660254f; // sqrt(3)/2
 
+        float xOffset = hexSize * 0.5f;
+        float zOffset = hexSize * 1.73f; // sqrt(3)/2
+
+        height = height / 2;
+        width = width * 2;
         for (int x = 0; x < width; x++)
         {
             for (int z = 0; z < height; z++)
             {
                 float xPos = x * xOffset;
                 float zPos = z * zOffset + (x % 2 == 1 ? zOffset / 2f : 0f);
-
                 Vector3 position = new Vector3(xPos, 0, zPos);
-                Instantiate(hexPrefab, position, Quaternion.identity, transform);
+
+                // Középre tesszük a kastélyt
+                if (x == width - 1 && z == height - 1)
+                {
+                    Instantiate(castleHexPrefab, position, Quaternion.identity, transform);
+                }
+                else
+                {
+                    Instantiate(groundHexPrefab, position, Quaternion.identity, transform);
+                }
             }
         }
     }
 }
+
+//float xOffset = hexSize * 0.5f;
+//float zOffset = hexSize * 1.73f; // sqrt(3)/2
