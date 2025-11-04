f51d5cb96a15adefa64c09104104899d11f3e548
gaaldod <gaaldod@gmail.com>
2025-11-04 21:17:00 +0100

    settings and volume sliders

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("UI Panels")]
+    public GameObject mainMenuPanel;
+    public GameObject settingsPanel;
+
+    void Start()
+	{
+		ShowMainMenu();
+    }
+    public void PlayGame()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+	public void QuitGame()
+	{
+		Application.Quit();
+    }
+
+    public void ShowSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void ShowMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+}
---
+++ Assets/Scripts/SettingsMenuManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class SettingsMenuManager : MonoBehaviour
+{
+    public Slider volume_master, volume_music, volume_effects;
+    public AudioMixer audioMixer;
+
+    public void ChangeMasterVolume()
+    {
+        audioMixer.SetFloat("volume_master", volume_master.value);
+    }
+    public void ChangeMusicVolume()
+    {
+        audioMixer.SetFloat("volume_music", volume_music.value);
+    }
+    public void ChangeEffectsVolume()
+    {
+        audioMixer.SetFloat("volume_effects", volume_effects.value);
+    }
+}
